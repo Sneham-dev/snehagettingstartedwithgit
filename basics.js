ec5d9e9fdcb431b948af98922c15ccd11e95ce10
Sneham-dev <snehamukkala.dev@gmail.com>
2021-11-03 18:13:55 +0530

    added basics file

---
+++ basics.js
@@ -0,0 +1,41 @@
+let s=2+3;
+console.log(s)
+
+let s='2'+3;
+console.log(s)
+
+let s='2'+'3';
+console.log(s);
+
+let s=2-3;
+console.log(s)
+
+let s='2'-3;
+console.log(s)
+
+let s=2-'3';
+console.log(s)
+
+let s='2'-'3';
+console.log(s)
+
+boolean
+let s='2'-true;
+console.log(s)
+
+let s='2'-false;
+console.log(s)
+
+let s='2'+true;
+console.log(s)
+
+let s='2'-'true';
+console.log(s)
+
+let s;
+s=4+undefined //nan
+
+s=true+undefined //nan
+
+
+let s=Number(s)  //output 5
